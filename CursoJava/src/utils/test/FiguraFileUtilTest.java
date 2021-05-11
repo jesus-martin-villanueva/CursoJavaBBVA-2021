b59b88a67a69314617b25d8ae038774b05571125
jesus-martin-villanueva <jesus.martin.villanueva@bbvaitspain.com>
2021-05-11 15:52:44 +0200

    subir practica figuras ficheros lectura y escritura

--- CursoJava/src/utils/test/FiguraFileUtilTest.java
+++ CursoJava/src/utils/test/FiguraFileUtilTest.java
@@ -2,14 +2,31 @@ package utils.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import repaso.Circulo;
+import repaso.Cuadrado;
+import repaso.Figura;
+import repaso.PoligonoRegular;
+import repaso.Rectangulo;
+import repaso.Triangulo;
+
 public class FiguraFileUtilTest {
+	
+	ArrayList<Figura> figuras;
 
 	@Before
 	public void setUp() throws Exception {
+		figuras = new ArrayList<Figura>();
+		figuras.add(new Cuadrado("cua1", 10));
+		figuras.add(new Circulo("cir1", 10f));
+		figuras.add(new Triangulo("tri1", 3f, 4f));
+		figuras.add(new Rectangulo("rec1", 10, 5));
+		figuras.add(new PoligonoRegular("pol1", 4f, 6, 5));
 	}
 
 	@After
@@ -17,8 +34,9 @@ public class FiguraFileUtilTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCalcularPerimetro_Cuadrado() {
+		//Resultado a esperar, método a probar, precisión
+		assertEquals(40, cuadrado.calcularPerimetro(), 0.1);
 	}
 
 }
